c98bca59c0de84c9ae703363ceb1054884d50fa5
zlchcipher000-web <zlchcipher000@gmail.com>
2026-06-16 07:46:06 +0000

    jh

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
+  base: '/User',
+  plugins: [react()],
   server: {
     port: 3000,
   },
